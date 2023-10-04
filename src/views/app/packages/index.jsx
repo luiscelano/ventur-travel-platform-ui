b23016291313d76055f648be9c83ee3df0e46d83
Crisddl <cdiazl6@miumg.edu.gt>
2023-10-03 20:37:22 -0600

    Packages
    cambie el index de paquetes y le agregue una hoja de estilos

--- src/views/app/packages/index.jsx
+++ src/views/app/packages/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Typography from 'src/components/Typography'
+import { Contenido } from './styles'
+import { boton } from './styles'
+import { formulario } from './styles'
 
 const Packages = () => {
   return (
-    <div>Paquetes</div>
+    <div>
+      <Contenido>
+        <Typography type="title" color="dark" size="large">
+          Paquetes de viaje
+        </Typography>
+      </Contenido>
+
+      <Contenido>
+        <Typography type="paragraph2">Consulta paquetes de viaje</Typography>
+      </Contenido>
+
+      <Contenido>
+       
+        <formulario>
+          <form action="" method="get">
+            <Typography type="paragraph1">
+              <input id="Nombre" type="text" placeholder="Nombre del paquete" />
+            </Typography>
+
+           
+            <boton>
+              <button>sss</button>
+            </boton>
+          </form>
+        </formulario>
+      </Contenido>
+    </div>
   )
 }
 
---
+++ src/views/app/packages/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Contenido = styled.div`
+text-align: center;
+margin-top: 20px; 
+margin-bottom: 20px;
+`
+
+export const formulario = styled.div`
+display: inline-block;
+`
+export const boton = styled.div`
+margin-left: 10px;
+`
+
+
