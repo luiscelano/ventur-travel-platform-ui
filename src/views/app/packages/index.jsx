29f804b014ff1e4bbd626a6fc0c430d71b1b901e
Crisddl <cdiazl6@miumg.edu.gt>
2023-10-03 22:27:23 -0600

    Vista paquetes consulta terminada
    vista paquetes consulta terminada

--- src/views/app/packages/index.jsx
+++ src/views/app/packages/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Typography from 'src/components/Typography'
 import { Contenido } from './styles'
-import { boton } from './styles'
-import { formulario } from './styles'
+import { Contenido1 } from './styles'
+import { Boton } from './styles'
+import { Tabla } from './styles'
+import { Form } from './styles'
+import { Input } from './styles'
+import { Tr } from './styles'
+import { Td } from './styles'
+import { Espacio } from './styles'
+import { Thead } from './styles'
 
 const Packages = () => {
   return (
@@ -17,21 +24,44 @@ const Packages = () => {
         <Typography type="paragraph2">Consulta paquetes de viaje</Typography>
       </Contenido>
 
-      <Contenido>
-       
-        <formulario>
-          <form action="" method="get">
-            <Typography type="paragraph1">
-              <input id="Nombre" type="text" placeholder="Nombre del paquete" />
+        
+          <Form>
+          <Contenido1>
+            <Typography type="paragraph2">
+              <Input id="Nombre"  type="text" placeholder="Nombre del paquete" />
             </Typography>
+            
+              <Boton><Typography type="paragraph2">BUSCAR</Typography></Boton>
+              
+            </Contenido1>
+      
+            </Form>
 
-           
-            <boton>
-              <button>sss</button>
-            </boton>
-          </form>
-        </formulario>
-      </Contenido>
+    <Espacio>
+    </Espacio>
+
+    <Contenido>
+    <Typography type="paragraph2">
+      <Tabla>
+      <Thead>
+      <Tr>
+        <Td>Nombre del Paquete </Td>
+        <Td> Decripcion</Td>
+        <Td>Cantidad de adultos </Td>
+        <Td>Cantidad de niños</Td>
+        <Td>Numero de noches</Td>
+        <Td>Hotel o lugar</Td>
+        <Td>precio</Td>
+      </Tr>
+      </Thead>
+      <Tr></Tr>
+      <Tr></Tr>
+      <Tr></Tr>
+
+      </Tabla>
+      </Typography>
+    </Contenido>
+
     </div>
   )
 }
--- src/views/app/packages/styles.js
+++ src/views/app/packages/styles.js
@@ -1,16 +1,69 @@
 import styled from 'styled-components'
 
 export const Contenido = styled.div`
+
 text-align: center;
 margin-top: 20px; 
 margin-bottom: 20px;
 `
 
-export const formulario = styled.div`
-display: inline-block;
-`
-export const boton = styled.div`
-margin-left: 10px;
+export const Contenido1 = styled.div`
+
+display: flex;
+flex-direction: row; 
+
 `
 
+export const Espacio = styled.div`
+margin-top: 50px;
+`
 
+export const Boton = styled.button`
+margin-left: 100px;
+border: 1px;
+border-radius: 15px;
+background-color: #A6D98F;
+padding: 5px 15px; 
+text-align: center;
+cursor: pointer; 
+`
+
+export const Tabla = styled.table`
+margin: 0 auto;
+border-collapse: collapse;
+`
+
+export const Td = styled.td`
+border: 1px solid #000000; 
+padding: 8px; 
+text-align: left;
+`
+
+export const Tr = styled.tr`
+border: 1px solid #000000; 
+padding: 8px; 
+text-align: left;
+`
+
+export const Thead = styled.thead`
+background-color: #F2EB80;
+`
+
+export const Form = styled.form`
+border: 1px;
+border-radius: 15px;
+background-color: #A6D98F;
+padding: 5px 5px; 
+width: 300px;
+margin: 0 auto;
+
+`
+
+export const Input = styled.input`
+border: 0.5px;
+border-color:#A6D98F;
+border-radius: 15px;
+background-color: #A6D98F;
+padding: 5px 5px; 
+width: 275px;
+`
