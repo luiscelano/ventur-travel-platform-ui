368dbeae32ad8c194138291fe06c4998387a3122
KillerFerd <91443717+KillerFerd@users.noreply.github.com>
2023-10-14 20:00:49 -0600

    Pequeños cambios

--- src/components/Field/index.jsx
+++ src/components/Field/index.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StyledInput from './styles';
 
-const Field = ({ type, size, color, ...props }) => {
+const Field = ({ type, size, color, pass, ...props }) => {
+  const isPassword = pass === 'true'; // Comprueba si pass es igual a "true"
   const disabled = type === 'disable';
-  return <StyledInput type="text" disabled={disabled} size={size} color={color} {...props} />;
+  return (
+    <StyledInput
+      type={isPassword ? 'password' : 'text'} // Establece el tipo según el valor de pass
+      disabled={disabled}
+      size={size}
+      color={color}
+      {...props}
+    />
+  );
 };
 
 Field.propTypes = {
   type: PropTypes.oneOf(['noDisable', 'disable']).isRequired,
   size: PropTypes.oneOf(['medium', 'large']).isRequired,
   color: PropTypes.oneOf(['background1', 'background2']).isRequired,
+  pass: PropTypes.oneOf(['true', 'false']), // Añade la validación para el atributo "pass"
 };
 
 export default Field;
