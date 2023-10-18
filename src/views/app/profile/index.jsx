157ea192105b20925a3ca58dc4521bb7af75238a
gabriela-giron <krodriguezg4@miumg.edu.gt>
2023-10-17 22:53:09 -0600

    cambios para mostrar datos de usuario

--- src/views/app/profile/index.jsx
+++ src/views/app/profile/index.jsx
@@ -3,24 +3,29 @@ import { Contenido } from './styles'
 import Typography from 'src/components/Typography'
 import Field from 'src/components/Field'
 import Button from 'src/components/Button'
+import { getProfile } from 'src/utils/storage'
 
 const Profile = () => {
+  const user = getProfile()
+  console.log('user ', user)
   return (
     <>
       <Contenido>
         <Typography type="title" color="dark">
-          Perfil, user
+          Perfil
         </Typography>
         <Typography type="paragraph2">Información del usuario</Typography>
-        <Field type="disable" size="large" color="background1"></Field>
-        <Field type="disable" size="large" color="background2"></Field>
-        <Field type="disable" size="large" color="background1"></Field>
-        <section>
+        <Field type="disable" size="large" color="background1">tipo de usuario: {user.permiso.descripcion}</Field>
+        <Field type="disable" size="large" color="background2">nombre completo: {user.nombre} {user.apellido}</Field>
+        <Field type="disable" size="large" color="background1">correo: {user.correo}</Field>
+        <Field type="disable" size="large" color="background2">fecha de ingreso: {user.fechaIngreso}</Field>
+        
+        {/* <section>
           <Field size="large" color="background2" placeholder="Nueva Contraseña"></Field>
           <Button size="small" color="background1">
             CAMBIAR
           </Button>
-        </section>
+        </section> */}
       </Contenido>
     </>
   )
