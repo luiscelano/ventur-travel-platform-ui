ecabcfc283c4b1a5bdef95f2e1db466a4a901a40
gabriela-giron <krodriguezg4@miumg.edu.gt>
2023-10-19 21:09:08 -0600

    correcion para perfil

--- src/views/app/profile/index.jsx
+++ src/views/app/profile/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as styles from './styles'
 import Typography from 'src/components/Typography'
-import info from 'src/components/userInfo'
+import InfoField from 'src/components/userInfo'
 import Button from 'src/components/Button'
 import { getProfile } from 'src/utils/storage'
 
@@ -14,17 +14,11 @@ const Profile = () => {
           Perfil
         </Typography>
         <Typography type="paragraph2">Información del usuario</Typography>
-        <info size="large" color="background1">{user.permiso.descripcion}</info>
-        <info size="large" color="background2">nombre completo: {user.nombre} {user.apellido}</info>
-        <info size="large" color="background1">correo: {user.correo}</info>
-        <info size="large" color="background2">fecha de ingreso: {user.fechaIngreso}</info>
+        <InfoField size="large" color="background1">permiso: {user.permiso.descripcion}</InfoField>
+        <InfoField size="large" color="background2">nombre completo: {user.nombre} {user.apellido}</InfoField>
+        <InfoField size="large" color="background1">correo: {user.correo}</InfoField>
+        <InfoField size="large" color="background2">fecha de ingreso: {user.fechaIngreso}</InfoField>
         
-        {/* <section>
-          <Field size="large" color="background2" placeholder="Nueva Contraseña"></Field>
-          <Button size="small" color="background1">
-            CAMBIAR
-          </Button>
-        </section> */}
       </styles.Contenido>
     </>
   )
